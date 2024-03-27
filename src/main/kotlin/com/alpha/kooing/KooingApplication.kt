351da82fc8d1b507736493270da8c26cebdee665
kym8821 <louie9798@naver.com>
2024-03-27 17:41:27 +0900

    [fefirst init

---
+++ src/main/kotlin/com/alpha/kooing/KooingApplication.kt
@@ -0,0 +1,11 @@
+package com.alpha.kooing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KooingApplication
+
+fun main(args: Array<String>) {
+	runApplication<KooingApplication>(*args)
+}
---
+++ src/test/kotlin/com/alpha/kooing/KooingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alpha.kooing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KooingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
