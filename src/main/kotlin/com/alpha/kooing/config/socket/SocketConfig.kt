0a35740c735d72e7442bac8c4e26273bb7b63196
kym8821 <louie9798@naver.com>
2024-05-05 22:32:33 +0900

    [feat] socket conifg

---
+++ src/main/kotlin/com/alpha/kooing/config/socket/SocketConfig.kt
@@ -0,0 +1,33 @@
+package com.alpha.kooing.config.socket
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class SocketConfig():WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOrigins("localhost:3000")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        // MessageMapping 매핑 prefix
+        registry.setApplicationDestinationPrefixes("/app")
+        // topic:여러 사용자에게 메세지 전송시 사용 (공지)
+        // queue:발행자에게 메세지 재전송시 사용
+        registry.enableSimpleBroker("/topic", "/queue")
+        // 특정 사용자에게 메세지 전송시 사용
+        registry.setUserDestinationPrefix("/user")
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        super.configureClientInboundChannel(registration)
+    }
+}
