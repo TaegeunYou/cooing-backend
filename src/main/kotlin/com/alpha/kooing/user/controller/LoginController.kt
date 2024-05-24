53a6c74610ddb2367ece87dfc41ac21ae6f6fcc3
kym8821 <louie9798@naver.com>
2024-05-24 17:42:54 +0900

    [feat] login 기능

--- src/main/kotlin/com/alpha/kooing/user/controller/LoginController.kt
+++ src/main/kotlin/com/alpha/kooing/user/controller/LoginController.kt
@@ -6,27 +6,54 @@ import com.alpha.kooing.config.jwt.JwtTokenProvider
 import com.alpha.kooing.user.Role
 import com.alpha.kooing.user.User
 import com.alpha.kooing.user.dto.CustomOAuth2User
+import com.alpha.kooing.user.dto.GoogleUserDto
 import com.alpha.kooing.user.dto.UserCreateDto
+import com.alpha.kooing.user.dto.UserLoginDto
 import com.alpha.kooing.user.enum.RoleType
+import com.alpha.kooing.user.service.LoginService
 import com.alpha.kooing.user.service.UserService
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.nimbusds.openid.connect.sdk.claims.UserInfo
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.boot.json.JsonParser
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.Base64
+import java.util.Objects
+import kotlin.math.log
 
 @RestController
 class LoginController(
     private val jwtTokenProvider: JwtTokenProvider,
     private val userService: UserService,
-    private val userManager: LoginUserManager
+    private val userManager: LoginUserManager,
+    private val loginService: LoginService
 ){
     val expiration:Long = 3600 * 60 * 60L
+
+    @PostMapping("/sign-in")
+    fun signIn(response: HttpServletResponse, @RequestBody loginDto:UserLoginDto): ApiResponse<*>{
+        try {
+            val token = loginDto.token
+            val tokenInfo = loginService.getLoginToken(token)?:throw Exception()
+            val userJwtToken = tokenInfo.getOrDefault("token", null)?:throw Exception()
+            val userRole = tokenInfo.getOrDefault("role", null)?:throw Exception()
+            response.setHeader("Set-Cookie", loginService.createCookieWithToken(userJwtToken).toString())
+            return ApiResponse("generate token success", userRole)
+        }catch (e:Exception) {
+            return ApiResponse("invalid token", Role.GUEST.name)
+        }
+    }
+
 
     @PostMapping("/signup")
     fun signup(request: HttpServletRequest, response: HttpServletResponse, @RequestBody userInfo:UserCreateDto) : ApiResponse<*>{
@@ -66,6 +93,7 @@ class LoginController(
     fun getLoginInfo(servletRequest: HttpServletRequest, response: HttpServletResponse): ApiResponse<*>{
         try {
             val token = jwtTokenProvider.resolveToken(servletRequest)
+            println(jwtTokenProvider.getJwtEmail(token))
             return ApiResponse(HttpStatus.OK.name, jwtTokenProvider.getJwtRole(token))
         }catch (e:Exception){
             return ApiResponse(HttpStatus.OK.name, Role.GUEST.name)
---
+++ src/main/kotlin/com/alpha/kooing/user/service/LoginService.kt
@@ -0,0 +1,75 @@
+package com.alpha.kooing.user.service
+
+import com.alpha.kooing.config.jwt.JwtTokenProvider
+import com.alpha.kooing.user.Role
+import com.alpha.kooing.user.dto.GoogleUserDto
+import com.alpha.kooing.user.repository.UserRepository
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.Cookie
+import org.springframework.http.ResponseCookie
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class LoginService(
+    private val userRepository: UserRepository,
+    private val jwtTokenProvider: JwtTokenProvider
+){
+
+    fun createCookieWithToken(token:String): ResponseCookie {
+        val cookie = ResponseCookie
+            .from("Authorization", token)
+            .path("/")
+            .maxAge(jwtTokenProvider.expiration)
+            .httpOnly(true)
+            .sameSite("None")
+            .secure(true)
+            .build()
+        return cookie
+    }
+
+    fun resolveTokenToObject(token:String):GoogleUserDto?{
+        try {
+            val payload = token.split(".")[1]
+            println(payload)
+            val decoder = Base64.getUrlDecoder()
+            val objectMapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            val userInfoJsonStr = String(decoder.decode(payload))
+            val userInfoJson = objectMapper.readValue(userInfoJsonStr, JsonNode::class.java)
+            return GoogleUserDto(
+                email = userInfoJson.get("email").asText(),
+                name = userInfoJson.get("name").asText(),
+            )
+        }catch (e:Exception){
+            return null
+        }
+    }
+
+    fun getLoginToken(token:String):MutableMap<String, String>?{
+        val userInfo = resolveTokenToObject(token)?:return null
+        val existsUser = userRepository.findByEmail(userInfo.email).orElse(null)
+        val res = mutableMapOf<String,String>()
+        if(existsUser != null) {
+            res["token"] = jwtTokenProvider.createJwt(
+                id = existsUser.id,
+                email = existsUser.email,
+                username = existsUser.username,
+                role = Role.USER.name,
+                expiration = jwtTokenProvider.expiration
+            )
+            res["role"] = Role.USER.name
+        }else{
+            res["token"] =  jwtTokenProvider.createJwt(
+                id = -1,
+                email = userInfo.email,
+                username = userInfo.name,
+                role = Role.LIMITED.name,
+                expiration = jwtTokenProvider.expiration
+            )
+            res["role"] = Role.LIMITED.name
+        }
+        return res
+    }
+}
