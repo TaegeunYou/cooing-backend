905993584519bbe6d0c0723d7025097a7b7d4800
kym8821 <louie9798@naver.com>
2024-06-05 20:13:53 +0900

    [fix] change date format

--- src/main/kotlin/com/alpha/kooing/util/DateUtil.kt
+++ src/main/kotlin/com/alpha/kooing/util/DateUtil.kt
@@ -7,13 +7,7 @@ import java.time.format.DateTimeFormatter
 class DateUtil {
     companion object{
         fun getDateTimeFormat(dateTime:LocalDateTime):String{
-            val date1 = dateTime.toLocalDate()
-            val date2 = LocalDate.now()
-            if(date1 != date2){
-                return dateTime.format(DateTimeFormatter.ofPattern("MM-dd"))
-            }else{
-                return dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
-            }
+            return dateTime.format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm"))
         }
     }
 }
